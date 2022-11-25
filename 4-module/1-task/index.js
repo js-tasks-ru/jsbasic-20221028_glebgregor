621170f59df518d8ecc492a9b550b374d5cb370a
Spark345 <90181596+Spark345@users.noreply.github.com>
2022-11-25 15:08:44 +0300

    module 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('UL')
+  // document.body.insertAdjacentElement('afterbegin', 'UL')
+  friends.map((item) =>{
+    let listItem = document.createElement('LI')
+    listItem.innerHTML = `${item.firstName} ${item.lastName}`;
+    list.append(listItem)
+  })
+  document.body.append(list)
+  return list;
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,12 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+let arr = [];
+  for(let i = 0; i < table.rows.length; i++){
+    let diagonalTdItem = table.rows[i].cells[i];
+    diagonalTdItem.style.backgroundColor = 'red';
+    arr.push(diagonalTdItem)
+  }
+  arr.forEach(item => {
+    return item
+  })
+
 }
