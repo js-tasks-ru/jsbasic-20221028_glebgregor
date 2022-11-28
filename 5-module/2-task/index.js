9da5f4d6ab720d35f59da640ce266d230a251e05
Spark345 <90181596+Spark345@users.noreply.github.com>
2022-11-28 14:58:20 +0300

    module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button')
+  let content = document.querySelector('#text')
+  btn.addEventListener('click', () =>{
+    console.log(content.hasAttribute('hidden'))
+    if(content.hasAttribute('hidden') === false){
+      content.setAttribute('hidden', '')
+    }else if(content.hasAttribute('hidden') === true){
+      content.removeAttribute('hidden')
+    }
+  })
+
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  let slider = document.querySelector('.carousel__inner')
+  let arrowRight = document.querySelector('.carousel__arrow_right')
+  let arrowLeft = document.querySelector('.carousel__arrow_left')
+
+  let styleTransformX = 0;
+  let i = 1;
+  let transformRight = () =>{
+    console.log(i)
+    if(i < slider.children.length){
+      slider.style.transform = `translateX(${styleTransformX -= slider.offsetWidth}px`
+      console.log(slider.style.transform)
+      i++;
+    }
+
+    if(i === slider.children.length){
+      arrowRight.style.display = 'none'
+      arrowLeft.style.display = ''
+    }
+  };
+  let transformLeft = () =>{
+    console.log(i)
+    if(i <= slider.children.length && i > 1){
+      slider.style.transform = `translateX(${styleTransformX += slider.offsetWidth}px`
+      console.log(slider.style.transform)
+      i--;
+    }
+    if(i === 1){
+      arrowLeft.style.display = 'none'
+      arrowRight.style.display = ''
+    }
+
+  };
+  if(i === 1){
+    arrowLeft.style.display = 'none'
+    arrowRight.style.display = ''
+  }
+  arrowRight.addEventListener('click', transformRight)
+  arrowLeft.addEventListener('click', transformLeft)
+
 }
